525572e2c213c9273279fe70ac879bff6e5a5e70
Ksoni-2311 <k2006soni@gmail.com>
2025-05-24 08:15:58 +0000

    fix: correct import paths for Header and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/header'
+import Header from './components/header.jsx'
 import Home from './components/home'
 import Project from './components/Projects'
 import Contactus from './components/Contactus'
 import OurPlans from './components/OurPlans'
-import Footer from './components/footer'
+import Footer from './components/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
